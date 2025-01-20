801003282ac850291bd7024d9406c8657af66489
kokombo <samuelibrahim3029@gmail.com>
2025-01-20 13:18:38 +0100

    Implemented mobile navbar component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,29 @@
+import MobileNavbar from "@/components/mobile-navbar";
+import Sidebar from "@/components/sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main className="flex h-screen w-full font-inter">{children}</main>;
+  const loggedIn = { firstName: "Sam", lastName: "Me" };
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="menu icon" width={30} height={30} />
+
+          <div>
+            <MobileNavbar user={loggedIn} />
+          </div>
+        </div>
+
+        {children}
+      </div>
+    </main>
+  );
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
 
-const Sidebar = () => {
-  return <aside>Sidebar</aside>;
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="logo"
+            className="size-6 max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Surge</h1>
+        </Link>
+
+        {sidebarLinks.map((link) => {
+          const isActive = pathname.startsWith(link.route);
+
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
