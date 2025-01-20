64c1a8252bd879ec615c03d41797848cd22ebc1c
kokombo <samuelibrahim3029@gmail.com>
2025-01-20 13:45:50 +0100

    mobile navbar

--- src/components/mobile-navbar.tsx
+++ src/components/mobile-navbar.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import {
   Sheet,
+  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const MobileNavbar = ({ user }: MobileNavProps) => {
+  const pathname = usePathname();
+
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
@@ -18,17 +24,59 @@ const MobileNavbar = ({ user }: MobileNavProps) => {
             width={30}
             height={30}
             alt="menu"
-            className="pointer-cursor"
+            className="cursor-pointer"
           />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+
+        <SheetContent side="left" className="border-none bg-white ">
+          <Link
+            href="/"
+            className="cursor-pointer flex items-center gap-1 px-4"
+          >
+            <Image src="/icons/logo.svg" width={34} height={34} alt="logo" />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              Surge
+            </h1>
+          </Link>
+
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((link) => {
+                  const isActive = pathname.startsWith(link.route);
+
+                  return (
+                    <SheetClose asChild key={link.label}>
+                      <Link
+                        href={link.route}
+                        className={cn("mobilenav-sheet_close w-full", {
+                          "bg-bank-gradient": isActive,
+                        })}
+                      >
+                        <Image
+                          src={link.imgURL}
+                          alt={link.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
+
+                        <p
+                          className={cn("text-16 font-semibold text-black-2", {
+                            "text-white": isActive,
+                          })}
+                        >
+                          {link.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
         </SheetContent>
       </Sheet>
     </section>
